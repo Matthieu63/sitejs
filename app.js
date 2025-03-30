bf67011b47634ab2c7782fb50edd9a62686c55a4
Matthieu63 <bonjour.matthieu@gmail.com>
2025-03-30 07:50:54 +0200

    ff

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.set('trust proxy', 1);
+
+
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ limit: '10mb' }));
